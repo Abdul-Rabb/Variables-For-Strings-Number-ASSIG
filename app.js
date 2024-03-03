399caa7d205e19d7721b86efddd6642ae363aa56
Abdul-Rabb <154547340+Abdul-Rabb@users.noreply.github.com>
2024-03-03 19:47:38 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,33 @@
+// 1 & 2. Declare a variable called username.
+ var username = 'abdul rabb';
+ console.log(username);
+
+//  3. Write script to
+// a) Declare a JS variable, titled message.
+// b) Assign “Hello World” to variable message
+// c) Display the message in alert box.
+ var title ="HELLO WORLD";
+ alert(title);
+    
+//  4. Write a script to save student’s bio data in JS variables and
+// show the data in alert boxes.
+
+var age = 25;
+var subject = "computer science";
+alert(username);
+alert(age);
+alert(subject);
+
+
+// 5. Write a script to display the following alert using one JS
+// variable:
+var pizza = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+alert(pizza);
+
+
+// 6. Declare a variable called email and assign to it a string that
+// represents your Email Address(e.g. example@example.com).
+// Show the blow mentioned message in an alert box.(Hint: use
+// string concatenation)
+var Email = "MY EMAIL ADDRESS IS abc@gmail.com";
+alert(Email);
